e0e7678326390f26fee5d8ba9846fb24d976c9e6
akbalki2108 <akbalki208012@gmail.com>
2024-03-21 20:15:00 +0530

    env change

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('~/DjangoApiMajorProject',)
+project_folder = os.path.expanduser('/home/aditya2108/DjangoApiMajorProject',)
 load_dotenv(os.path.join(project_folder, '.env'))
 
 
